827daf6d617e157529af36109fdcffd73c553003
Hsieh-Ting Lin <ppoiu87@gmail.com>
2016-09-12 18:49:07 +0800

    go

---
+++ ArticleClass.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleClass.swift
+//  HealthEdu
+//
+//  Created by Mac on 2016/9/11.
+//  Copyright © 2016年 NCKU_hospital. All rights reserved.
+//
+
+import Foundation
+class article {
+    var title : String
+    var photo : String
+    var author : String
+    var body : String
+    
+}
---
+++ HealthEdu/article.swift
@@ -0,0 +1,13 @@
+//
+//  article.swift
+//  HealthEdu
+//
+//  Created by Mac on 2016/9/11.
+//  Copyright © 2016年 NCKU_hospital. All rights reserved.
+//
+
+import Cocoa
+
+class article: UIViewController {
+
+}
