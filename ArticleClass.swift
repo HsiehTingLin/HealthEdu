226366e0dad91375c1e0034e36bad11d60aca266
Hsieh-Ting Lin <ppoiu87@gmail.com>
2016-09-13 19:18:55 +0800

    the newest update at 0913 19:20

--- ArticleClass.swift
+++ ArticleClass.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Mac on 2016/9/11.
 //  Copyright © 2016年 NCKU_hospital. All rights reserved.
-//
+//  目前還有 division、time的 var還沒加，如果要幫每個文章加的話就要加在這裡
 
 import Foundation
 class article {
@@ -12,5 +12,11 @@ class article {
     var photo : String
     var author : String
     var body : String
+    init (title: String, photo : String, author : String, body : String){
+        self.title = title
+        self.photo = photo
+        self.author = author
+        self.body = body
+    }
     
 }
