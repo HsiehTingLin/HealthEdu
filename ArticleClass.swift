3361be0f73055d5eecd9bdbd3893551ea5f8334b
Yu-Ju Lin <nps798@gmail.com>
2016-09-21 00:01:37 +0800

    從你的檔案改東改西 0930 前應該就是教這個架構 只是再微調一些東西 這裡加入 QR Code 產生器 然後我將 article class 新增兩元素  id 跟 photoUIImage （你在宣告的時候 直接用 UIImage() 空元素呼攏過去就好） topic Class 我也刪掉一些不用的元素 DivisionViewController 我也新增 成大醫院 這個section 底下有最新消息這個 division 除此之外，改最多的是 StarMany / StarSingleTopic.swift StarMany 會知道我們點選了哪個 cell 把 cell 的編號 pass 到 StarSingletopic 這裡面 StarSingleTopic 裡面有一個很複雜的 array (我早上在成杏廳給你看的那個) 這個複雜array就會根據編號 顯示不同的文章
    所以.... 如果要在 分科 那邊也做到「按下不同分科 顯示不同文章列表」就可以學 StarSingleTopic 這邊的做法
    其他等待新增：
    1. QR Code Reader (無法在電腦虛擬機測試喔 一定要實機)
    2. Core Data 初步功能

--- ArticleClass.swift
+++ ArticleClass.swift
@@ -7,16 +7,30 @@
 //  目前還有 division、time的 var還沒加，如果要幫每個文章加的話就要
 
 import Foundation
+import UIKit
+
 class article {
+    
+    var id : String
+    // 新增id，String先用Int好了，因為我很懶惰，到時候要轉 int 也不難就是了
+    
     var title : String
     var photo : String
+    var photoUIImage : UIImage
+    // 新增 photoUIImage 因為在 未來圖片是當場從網路下載，下載完成會立刻將它轉換成 UIImage
+    // 所以需要有一個變數儲存這個
+    
     var author : String
     var body : String
     var time : String
     var division : String
-    init (title: String, photo : String, author : String, body : String, time: String, division: String){
+    
+    init (id: String, title: String, photoUIImage : UIImage, photo : String, author : String, body : String, time: String, division: String){
+        
+        self.id = id
         self.title = title
         self.photo = photo
+        self.photoUIImage = photoUIImage
         self.author = author
         self.body = body
         self.time = time
--- HealthEdu/ArticleViewController.swift
+++ HealthEdu/ArticleViewController.swift
@@ -28,9 +28,79 @@ class ArticleViewController: UIViewController {
     var currentTimeString = ""
     //這裡先宣告一些變數，注意這裡的名字會被使用在資料來源的vc
     
+    
+    // 以下為 qrcodeImage 變數
+    var qrcodeImage: CIImage!
+    
+    // 以下這個也是要傳遞過來的變數
+    var articleIdforQRCode :String? = "http://myelin.tk/for_ncku_app_test/"
+    
+    
     @IBAction func qrcodeBtn(sender: AnyObject) {
-        // Qrcode 功能放在這裡
+        let alertMessage = UIAlertController(title: "請使用QR Code掃描器掃描！", message: "點擊背景以返回", preferredStyle: .Alert)
+        
+        let height:NSLayoutConstraint = NSLayoutConstraint(item: alertMessage.view, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: self.view.frame.height * 0.45)
+        
+        alertMessage.view.addConstraint(height)
+        
+        
+        
+        let imageView = UIImageView(frame: CGRectMake(35, (self.view.frame.height*0.45-200)/2+20, 200, 200))
+        
+        imageView.image = self.GenerateQRCode(imageView)
+        // 右邊產生 qrcode
+        alertMessage.view.addSubview(imageView)
+        
+        //let action = UIAlertAction(title: "返回", style: .Default, handler: nil)
+        
+        //alertMessage.addAction(action)
+        
+        self.presentViewController(alertMessage, animated: true, completion:{
+            alertMessage.view.superview?.userInteractionEnabled = true
+            alertMessage.view.superview?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.alertControllerBackgroundTapped)))
+        })
+
     }
+    
+    // 產生QR Code
+    func GenerateQRCode(imgview: UIImageView) -> UIImage
+    {
+        
+        if self.articleIdforQRCode == "" {
+            return UIImage()
+        }
+        
+        let data = self.articleIdforQRCode!.dataUsingEncoding(NSISOLatin1StringEncoding, allowLossyConversion: false)
+        // data 為要送去製造 QR Code 的 String
+        
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        
+        filter!.setValue(data, forKey: "inputMessage")
+        filter!.setValue("Q", forKey: "inputCorrectionLevel")
+        
+        self.qrcodeImage = filter!.outputImage
+        
+        let scaleX = imgview.frame.size.width / self.qrcodeImage.extent.size.width
+        let scaleY = imgview.frame.size.height / self.qrcodeImage.extent.size.height
+        // 上面這兩行 是算出目前 imgQRCode 這個 frame 的大小與 qrcodeimage
+        
+        let transformedImage = self.qrcodeImage.imageByApplyingTransform(CGAffineTransformMakeScale(scaleX, scaleY))
+        // 轉換 qrcodeimage的大小
+        
+        return UIImage(CIImage: transformedImage)
+        
+    }
+    
+    
+    // 按下背景可以返回
+    func alertControllerBackgroundTapped()
+    {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         currentPhoto.image = UIImage(named: currentPhotoString)
